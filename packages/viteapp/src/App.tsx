487f3e9294127b42cb163432091de8ad84e8392e
abdelmomen1985 <abdelmomen1985@gmail.com>
2022-05-16 21:04:25 +0000

    tailwind working

--- packages/viteapp/src/App.tsx
+++ packages/viteapp/src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { add } from '@knola-2/utils'
+import { add } from "@knola-2/utils";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      {add(5,2)}
-    </div>
-  )
+    <>
+      <div className="bg-gray-700">
+
+        <h1 className="text-5xl text-orange-800">{add(5, 2)}</h1>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
